99b6cca723e89db91eaa1e9facb820ab5b44e1bc
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2020-08-30 15:39:46 +0300

    добавил файл(модуль) для экспорта/импорта

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const header = 'Заголовок 42';
-const num = 11;
-const a = 2;
-const b = 3;
-const c = 5;
-const d = 10;
-
-
+import * as nums from './number';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p style={{
-          color: 'green',
-          fontSize: '38px'
-        }}>
-           Пример добавления в разметку объекта style
-        </p>
-        <h1>
-        Пример вывода значения переменной: {header}
-        </h1>
-        <p>Пример вывода числа: {num}</p>
-        <p>Пример вывода результата арифметической операции: { 12+11-7 }</p>
-        <p>Пример вывода результата логической операции: { a && b }</p>
-        <p>Пример использования тернарного условия: { c > d ? c : d }</p>
-        <p>Пример вывода undefined, null, false и true: {undefined} {null} {false} {true} </p>
+      <p>{nums.count * nums.length}</p>
       </header>
 
     </div>
---
+++ src/number.js
@@ -0,0 +1,5 @@
+
+const count = 42;
+const length = 24;
+
+export {count, length}
