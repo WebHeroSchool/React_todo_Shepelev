4413151fb02ca97a0fa03d57be7b3ef399608737
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2020-11-03 15:09:59 +0300

    добавил input

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import styles from './InputItem.module.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(0),
-      width: '500px',
-      borderColor: 'green',
+      width: '500px', 
     },
   },
 }));
@@ -15,12 +15,14 @@ const useStyles = makeStyles((theme) => ({
 export default function InputItem() {
   const classes = useStyles();
 
+//   return (
+//     <form className={classes.root} noValidate autoComplete="off">
+//       <TextField  id="outlined-basic" label="Что нужно сделать?" variant="outlined" />
+//       <input className={styles.input} placeholder="Что нужно сделать?" type="text"></input>
+//     </form>
+//   );
+
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Что нужно сделать?" variant="outlined" />
-    </form>
+    <input className={styles.input} placeholder="Что нужно сделать?" type="text"></input>
   );
 }
-
-
-
