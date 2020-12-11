8cda93742b1a606564209176853172977925116f
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2020-12-11 15:12:11 +0300

    добавил обработчик для  кнопки  add

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import styles from './InputItem.module.css';
+import React from "react";
+import styles from "./InputItem.module.css";
 
-
-export default function InputItem() {
-
+export default function InputItem({ onClickAdd }) {
   return (
-
-     <form>
-      <input className = { styles.text } type = "text" placeholder= "Запишите дело" />
-      <button className = { styles.btn } >Add</button>
-
+    <form>
+      <input className={styles.text} type="text" placeholder="Запишите дело" />
+      <button className={styles.btn} onClick={onClickAdd}>
+        Add
+      </button>
     </form>
   );
 }
-
-
-
