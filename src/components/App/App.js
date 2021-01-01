db3223ee66a5638e5859a1f9a6c6ab493d050a83
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-01 20:27:30 +0300

    изменил компонент app; добавил use state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,90 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import InputItem from "../InputItem/InputItem";
 import ItemList from "../ItemList/ItemList";
 import Footer from "../Footer/Footer";
 import styles from "./App.module.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onClickDone = this.onClickDone.bind(this);
-    this.onClickDelete = this.onClickDelete.bind(this);
-    this.onClickAdd = this.onClickAdd.bind(this);
-    this.state = {
-      taskCount: 4,
-      importantTask: [
-        {
-          value: "Создать приложение",
-          id: 1,
-          isDone: false,
-        },
-        {
-          value: "Исправить баги",
-          id: 2,
-          isDone: false,
-        },
-        {
-          value: "Отпраздновать!",
-          id: 3,
-          isDone: false,
-        },
-        {
-          value: "Опубликовать",
-          id: 4,
-          isDone: false,
-        },
-      ],
-    };
-  }
+const App = () => {
+  const initialState = {
+    taskCount: 4,
+    importantTask: [
+      {
+        value: "Создать приложение",
+        id: 1,
+        isDone: false,
+      },
+      {
+        value: "Исправить баги",
+        id: 2,
+        isDone: false,
+      },
+      {
+        value: "Отпраздновать!",
+        id: 3,
+        isDone: false,
+      },
+      {
+        value: "Опубликовать",
+        id: 4,
+        isDone: false,
+      },
+    ],
+  };
 
-  onClickDone = (id) => {
-    const newItemList = this.state.importantTask.map((item) => {
+  const [todoItem, setTodoItem] = useState(initialState.importantTask);
+  const [count, setCount] = useState(initialState.taskCount);
+
+  const onClickDone = (id) => {
+    const newItemList = todoItem.map((item) => {
       const newItem = { ...item };
       if (item.id === id) {
         newItem.isDone = !item.isDone;
       }
       return newItem;
     });
-    this.setState({ importantTask: newItemList });
+    setTodoItem({ importantTask: newItemList });
   };
 
-  onClickDelete = (id) =>
-    this.setState((state) => ({
+  const onClickDelete = (id) =>
+    setTodoItem((state) => ({
       importantTask: state.importantTask.filter(
         (importantTask) => importantTask.id !== id
       ),
-      taskCount: state.taskCount - 1,
+      count: setCount((count) => count - 1),
     }));
 
-  onClickAdd = (value) =>
-    this.setState((state) => ({
+  const onClickAdd = (value) =>
+    setTodoItem((state) => ({
       importantTask: [
         ...state.importantTask,
         {
           value: value,
-          id: state.taskCount + 1,
+          id: setCount((count) => count + 1),
           isDone: false,
         },
       ],
-      taskCount: state.taskCount + 1,
+      count: setCount((count) => count + 1),
     }));
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <h1 className={styles.title}> todo's list </h1>
-        <InputItem onClickAdd={this.onClickAdd} />
-        <hr className={styles.line} />
-        <ItemList
-          todoItem={this.state.importantTask}
-          onClickDone={this.onClickDone}
-          onClickDelete={this.onClickDelete}
-        />
-        <hr className={styles.line} />
-        <Footer count={this.state.taskCount} />
-      </div>
-    );
-  }
-}
-
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}> todo's list </h1>
+      <InputItem onClickAdd={onClickAdd} />
+      <hr className={styles.line} />
+      <ItemList
+        todoItem={todoItem}
+        onClickDone={onClickDone}
+        onClickDelete={onClickDelete}
+      />
+      <hr className={styles.line} />
+      <Footer count={count} />
+    </div>
+  );
+};
 export default App;
