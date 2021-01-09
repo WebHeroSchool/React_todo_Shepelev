81cb09bf0833fab4d890f944b7136c02da14e747
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-09 18:24:14 +0300

    сделал стили для меню

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,5 +2,5 @@ import React from "react";
 // import styles from "./About.module.css";
 
 export default function About() {
-  return <div>Обо мне</div>;
+  return <div className="about__container">Обо мне</div>;
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import styles from "./App.module.css";
+import styles from "./App.module.css";
 import About from "../About/About";
 import Contacts from "../Contacts/Contacts";
 import Todo from "../Todo/Todo";
 
 const App = () => (
   <Router>
-    <div>
-      <Link to="/">Обо мне</Link>
-      <Link to="/todo">Список дел</Link>
-      <Link to="/contacts">Контакты</Link>
+    <div className={styles.menu__container}>
+      <div className={styles.menu__link}>
+        <Link className={styles.menu__item} to="/">
+          Обо мне
+        </Link>
+        <Link className={styles.menu__item} to="/todo">
+          Список дел
+        </Link>
+        <Link className={styles.menu__item} to="/contacts">
+          Контакты
+        </Link>
 
-      <Route path="/" exact component={About} />
-      <Route path="/todo" component={Todo} />
-      <Route path="/contacts" component={Contacts} />
+        <div className={styles.menu__content}></div>
+        <Route path="/" exact component={About} />
+        <Route path="/todo" component={Todo} />
+        <Route path="/contacts" component={Contacts} />
+      </div>
     </div>
   </Router>
 );
