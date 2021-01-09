463c9ff0645018e708ef5aec6fb42c1a20903f62
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-09 15:48:51 +0300

    добавил меню, линки и роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 // import styles from "./App.module.css";
 import About from "../About/About";
 import Contacts from "../Contacts/Contacts";
 import Todo from "../Todo/Todo";
 
-export default function App() {
-  return (
+const App = () => (
+  <Router>
     <div>
-      <About />
-      <Contacts />
-      <Todo />
+      <Link to="/">Обо мне</Link>
+      <Link to="/todo">Список дел</Link>
+      <Link to="/contacts">Контакты</Link>
+
+      <Route path="/" exact component={About} />
+      <Route path="/todo" component={Todo} />
+      <Route path="/contacts" component={Contacts} />
     </div>
-  );
-}
+  </Router>
+);
+
+export default App;
