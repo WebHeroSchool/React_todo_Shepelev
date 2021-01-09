81c03ad6b0e248a1bb694888bb1e0af64ffe3f7c
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-09 15:25:56 +0300

    сделал рефакторинг; создал заглушки; переписал импорты

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/components/App/App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../src/components/App/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
