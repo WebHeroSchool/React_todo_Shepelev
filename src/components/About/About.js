9aa55062defd7a538228d478827d947bc78e3db0
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-09 09:28:39 +0300

    created request and view repositories list

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,42 @@
 import React from "react";
 // import styles from "./About.module.css";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit();
 
 export default class About extends React.Component {
   state = {
     isLoading: true,
+    repoList: [],
   };
+
+  componentDidMount() {
+    octokit.repos
+      .listForUser({
+        username: "EPShepelev",
+      })
+      .then(({ data }) => {
+        console.log(data);
+        this.setState({
+          repoList: data,
+          isLoading: false,
+        });
+      });
+  }
+
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, repoList } = this.state;
     return (
       <div className="about__container">
-        {isLoading ? <CircularProgress /> : "Обо мне"}
+        <div>{isLoading ? <CircularProgress /> : "Мои репозитории"}</div>
+        {!isLoading && (
+          <ol>
+            {repoList.map((repo) => (
+              <li key={repo.id}>{repo.name}</li>
+            ))}
+          </ol>
+        )}
       </div>
     );
   }
